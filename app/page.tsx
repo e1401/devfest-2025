e837319684a008141b7cd40d360406d980c98451
Boris Martinovic <boris@martinovic.dev>
2023-08-19 17:59:24 +0200

    Changed font

--- app/page.tsx
+++ app/page.tsx
@@ -8,31 +8,33 @@ export default function MainPage() {
       <div className="mx-auto  sm:mt-0 sm:grid-cols-3 lg:gap-16">
         <div className="p-4 relative flex flex-col items-center gap-4 duration-700 group md:gap-8 md:py-24  lg:pb-48  md:p-16">
           <div className="z-10 flex flex-col items-center">
-            <div className="mt-[30vh] mx-auto font-medium duration-150 lg:text-5xl font-display text-3xl">
+            <div className="mt-[30vh] mx-auto font-medium duration-150 lg:text-6xl font-display text-3xl">
               DEVFEST 2023
             </div>
-            <div className="mt-4 mb-16 text-lg lg:text-xl text-center duration-1000  dark:group-hover:text-slate-200 group-hover:text-slate-500 flex flex-col gap-3">
+            <div className="mt-4 mb-16 text-lg lg:text-2xl text-center duration-1000  dark:group-hover:text-slate-200 group-hover:text-slate-500 flex flex-col gap-3">
               &lt; Zagreb, 27th October 2023 &gt;
-              <Link
-                target="_blank"
-                href={
-                  "https://docs.google.com/forms/d/e/1FAIpQLScrrAm6aIN9C2WPPI3Ir1xQmtXz67SOE-qhE97YarNaPFiq9w/viewform?pli=1"
-                }
-              >
-                <Button className="mt-3 w-[220px]">
-                  <Mic2 className="h-4 w-4 lg:mr-2"></Mic2>
-                  Apply to be a Speaker!
-                </Button>
-              </Link>
-              <Link
-                href="https://www.entrio.hr/event/devfest-2023-14840"
-                target="_blank"
-              >
-                <Button className="mt-3 w-[220px]">
-                  <Ticket className="h-4 w-4 lg:mr-2"></Ticket>
-                  <div className="hidden lg:inline">Get Tickets</div>
-                </Button>
-              </Link>
+              <div className="flex gap-x-5">
+                <Link
+                  target="_blank"
+                  href={
+                    "https://docs.google.com/forms/d/e/1FAIpQLScrrAm6aIN9C2WPPI3Ir1xQmtXz67SOE-qhE97YarNaPFiq9w/viewform?pli=1"
+                  }
+                >
+                  <Button className="mt-3 w-[190px]">
+                    <Mic2 className="h-4 w-4 lg:mr-2"></Mic2>
+                    Become a Speaker!
+                  </Button>
+                </Link>
+                <Link
+                  href="https://www.entrio.hr/event/devfest-2023-14840"
+                  target="_blank"
+                >
+                  <Button className="mt-3 w-[190px]">
+                    <Ticket className="h-4 w-4 lg:mr-2"></Ticket>
+                    <div className="hidden lg:inline">Get Tickets</div>
+                  </Button>
+                </Link>
+              </div>
             </div>
           </div>
         </div>
