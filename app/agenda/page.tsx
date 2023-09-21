1c3cf9b9421194e8bb60a89aaac3e6509e5d277c
Boris Martinović <boris@martinovic.dev>
2023-09-21 23:51:45 +0200

    Generalized the speaker object and created JSON

--- app/agenda/page.tsx
+++ app/agenda/page.tsx
@@ -1,27 +1,28 @@
 import AgendaCard from '@/components/agenda-card';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import stages from '@/json/speakers.json';
 
-export default function Agenda() {
+export default async function Agenda() {
   return (
     <div className="container flex flex-col items-center px-4 mx-auto min-h-screen">
       <h1 className="mt-3 text-3xl font-bold">Event agenda</h1>
       <Tabs
-        defaultValue="track1"
+        defaultValue="stage1"
         className="w-full mx-auto items-center flex flex-col mt-2"
       >
         <TabsList className="mx-auto">
-          <TabsTrigger value="track1">Stage 1</TabsTrigger>
-          <TabsTrigger value="track2">Stage 2</TabsTrigger>
+          <TabsTrigger value="stage1">Stage 1</TabsTrigger>
+          <TabsTrigger value="stage2">Stage 2</TabsTrigger>
         </TabsList>
-        <TabsContent value="track1">
-          <AgendaCard />
-          <AgendaCard />
+        <TabsContent value="stage1">
+          {stages.stage1.map((talk) => {
+            return <AgendaCard talk={talk} />;
+          })}
         </TabsContent>
-        <TabsContent value="track2">
-          <AgendaCard />
-          <AgendaCard />
-          <AgendaCard />
-          <AgendaCard />
+        <TabsContent value="stage2">
+          {stages.stage2.map((talk) => {
+            return <AgendaCard talk={talk} />;
+          })}
         </TabsContent>
       </Tabs>
     </div>
--- components/agenda-card.tsx
+++ components/agenda-card.tsx
@@ -2,29 +2,36 @@ import React from 'react';
 import { Separator } from './ui/separator';
 import Image from 'next/image';
 
-export default function AgendaCard() {
+export default function AgendaCard(props: {
+  talk: {
+    time: string;
+    name: string;
+    imgSrc: string;
+    info: string;
+    talkName: string;
+    talkInfo: string;
+  };
+}) {
   return (
     <>
       <Separator className="my-4" />
       <div className="flex flex-col lg:flex-row items-center lg:gap-x-12 gap-y-3 ">
-        <h1 className="text-3xl font-semibold">11:00</h1>
+        <h1 className="text-3xl font-semibold">{props.talk.time}</h1>
         <div className="flex items-center gap-x-3">
           <Image
             className="rounded-full"
-            src={'/speaker-photos/Boris.jpg'}
-            alt={'Boris Martinovic'}
+            src={props.talk.imgSrc}
+            alt={props.talk.name}
             height={100}
             width={100}
           ></Image>
           <div className="flex flex-col gap-3">
-            <h2 className="text-2xl font-bold">Boris Martinovic</h2>
-            <h5 className="text-sm">Software enginneer @ Euroherc Insurance</h5>
+            <h2 className="text-2xl font-bold">{props.talk.name}</h2>
+            <h5 className="text-sm">{props.talk.info}e</h5>
           </div>
         </div>
         <div className="flex lg:flex-col max-w-[50vw] gap-3">
-          <h2 className="text-2xl font-bold">
-            From Punch Cards to Predictive Coding
-          </h2>
+          <h2 className="text-2xl font-bold">{props.talk.talkName}</h2>
           <h5 className="text-sm">
             We will go through the history of development workflow and how it
             could change in the near future. First, we will go through some of
