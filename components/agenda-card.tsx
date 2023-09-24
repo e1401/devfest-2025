c19fcf69373e342f65d050c073f7e1a74a7ca00c
Boris Martinović <boris@martinovic.dev>
2023-09-24 20:36:03 +0200

    New look for speaker section

--- components/agenda-card.tsx
+++ components/agenda-card.tsx
@@ -2,6 +2,12 @@ import React, { Suspense } from 'react';
 import { Separator } from './ui/separator';
 import Image from 'next/image';
 import { Skeleton } from './ui/skeleton';
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from '@/components/ui/accordion';
 
 export default function AgendaCard(props: {
   talk: {
@@ -15,33 +21,48 @@ export default function AgendaCard(props: {
 }) {
   return (
     <>
-      <Separator className="my-4" />
-      <div className="flex flex-col lg:flex-row items-center lg:gap-x-12 gap-y-3 ">
-        <h1 className="text-3xl font-semibold w-[150px] text-center">
-          {props.talk.time}
-        </h1>
-        <div className="flex items-center gap-x-3 w-[350px]">
-          <Suspense
-            fallback={<Skeleton className="w-[100px] h-[100px] rounded-full" />}
-          >
-            <Image
-              className="rounded-full"
-              src={props.talk.imgSrc}
-              alt={props.talk.name}
-              height={100}
-              width={100}
-            ></Image>
-          </Suspense>
-          <div className="flex flex-col gap-3">
-            <h2 className="text-2xl font-bold">{props.talk.name}</h2>
-            <h5 className="text-sm">{props.talk.info}</h5>
-          </div>
-        </div>
-        <div className="flex flex-col max-w-[50vw] gap-3">
-          <h2 className="text-2xl font-bold">{props.talk.talkName}</h2>
-          <h5 className="text-sm">{props.talk.talkInfo}</h5>
-        </div>
-      </div>
+      <Accordion
+        type="single"
+        collapsible
+        className="w-full border p-3 rounded-lg my-3 bg-primary-foreground/20"
+      >
+        <AccordionItem value="item">
+          <AccordionTrigger>
+            <div className="flex flex-col lg:flex-row items-center lg:gap-x-12 gap-y-3 w-full">
+              <h1 className="text-3xl font-semibold text-left lg:text-center w-[150px]">
+                {props.talk.time}
+              </h1>
+              <div className="flex items-center gap-x-3 w-[350px]">
+                <Suspense
+                  fallback={
+                    <Skeleton className="w-[100px] h-[100px] rounded-full" />
+                  }
+                >
+                  <Image
+                    className="rounded-full"
+                    src={props.talk.imgSrc}
+                    alt={props.talk.name}
+                    height={100}
+                    width={100}
+                  ></Image>
+                </Suspense>
+                <div className="flex flex-col gap-3">
+                  <h2 className="text-2xl font-bold">{props.talk.name}</h2>
+                  <h5 className="text-sm">{props.talk.info}</h5>
+                </div>
+              </div>
+              <div className="flex flex-col max-w-[50vw] gap-3">
+                <h2 className="text-2xl font-bold">{props.talk.talkName}</h2>
+              </div>
+            </div>
+          </AccordionTrigger>
+          <AccordionContent>
+            <div className="flex flex-col lg:flex-row items-center lg:gap-x-12 gap-y-3 border p-3 rounded-lg">
+              <h5 className="text-sm">{props.talk.talkInfo}</h5>
+            </div>
+          </AccordionContent>
+        </AccordionItem>
+      </Accordion>
     </>
   );
 }
