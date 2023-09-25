10622ee6b3b11932f928c1d03b086b29e79e2e2b
Boris Martinovic <boris@martinovic.dev>
2023-09-25 21:51:54 +0200

    new landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -47,9 +47,11 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <SiteHeader />
-          <div className="flex-1 bg-image-light dark:bg-image-dark">
-            <div className="flex-1">{children}</div>
-            <SiteFooter />
+          <div className=" bg-image-light dark:bg-image-dark">
+            <div className="flex-1">
+              <div className="flex-1">{children}</div>
+              <SiteFooter />
+            </div>
           </div>
         </ThemeProvider>
       </body>
