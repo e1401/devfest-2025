668f83fb3cd30515d1556814d172e0da73896e9a
Antonio Hadrović <38010322+e1401@users.noreply.github.com>
2025-07-25 21:26:25 +0200

    Update the year in meta (#51)
    Co-authored-by: Antonio Hadrovic <ahadrovic@Macbook-Pro-2017.local>

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export const siteConfig = {
-    name: 'GDG Croatia DevFest 2024',
+    name: 'GDG Croatia DevFest 2025',
     url: 'https://gdg.community.dev/gdg-zagreb/',
-    description: 'Website for the upcoming DevFest 2024 conference',
+    description: 'Website for the upcoming DevFest 2025 conference',
     links: {
         twitter: 'https://twitter.com/gdgzagreb',
         github: 'https://github.com/GDG-Zagreb',
